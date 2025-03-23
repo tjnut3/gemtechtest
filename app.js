ba126ddfe1e335f87feb08ae1130f81bcf217052
tjnut3 <notakcoblet@gmail.com>
2025-03-23 18:38:55 +0700

    again

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.set("trust proxy", false);
+app.set("trust proxy", 1);
 
 //? Sessions
 app.use(
--- modules/ratelimit/authRatelimiter.js
+++ modules/ratelimit/authRatelimiter.js
@@ -5,9 +5,12 @@ const RedisStore = require('rate-limit-redis');
 const redis = require('../database/redis');
 
 const registerRateLimiter = rateLimit({
-   
+    /*store: new RedisStore({
+        sendCommand: (...args) => redis.sendCommand(args),
+    }),
+    */
     windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 1000, // limit each IP to 3 requests per window Ms
+    max: 5, // limit each IP to 3 requests per window Ms
     standardHeaders: true,
     legacyHeaders: false,
     message: "Too many accounts created, please try again in 10 minutes"
@@ -19,7 +22,7 @@ const loginRateLimiter = rateLimit({
         return req.ip;
     },
     windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 1000, // limit each IP to 10 requests per window Ms
+    max: 5, // limit each IP to 10 requests per window Ms
     standardHeaders: true,
     legacyHeaders: false,
     message: "Too many attempt to login, please try again in 10 minutes"
