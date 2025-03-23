1b7c5f2c329c4d5c07ea34bd47a1f8c0910830db
tjnut3 <notakcoblet@gmail.com>
2025-03-23 18:20:46 +0700

    Ratelimit

--- modules/ratelimit/authRatelimiter.js
+++ modules/ratelimit/authRatelimiter.js
@@ -5,10 +5,7 @@ const RedisStore = require('rate-limit-redis');
 const redis = require('../database/redis');
 
 const registerRateLimiter = rateLimit({
-    /*store: new RedisStore({
-        sendCommand: (...args) => redis.sendCommand(args),
-    }),
-    */
+   
     windowMs: 10 * 60 * 1000, // 10 minutes
     max: 1000, // limit each IP to 3 requests per window Ms
     standardHeaders: true,
@@ -17,10 +14,10 @@ const registerRateLimiter = rateLimit({
 });
 
 const loginRateLimiter = rateLimit({
-    /*store: new RedisStore({
-        sendCommand: (...args) => redis.sendCommand(args),
-    }),
-    */
+    keyGenerator: (req) => {
+        console.log("Request IP:", req.ip);  // พิมพ์ IP ที่ได้ออกมา
+        return req.ip;
+    },
     windowMs: 10 * 60 * 1000, // 10 minutes
     max: 1000, // limit each IP to 10 requests per window Ms
     standardHeaders: true,
